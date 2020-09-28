c82d9e426ef64f20c925569a69dadcd950d2d941
WLNewman <68798389+WLNewman@users.noreply.github.com>
2020-09-28 00:36:31 -0700

    Update cam.py
    Changed things up a bit a bit to steamline things. This now uses pigpio to find the daemon instead of OS. Has a problem of taking forever because it gets stuck in loop of rotating motor. Will fix soon, either by multithreading or pushing that onto the Pi (definitely the latter since it'll be less intensive on the computer's CPU)

--- cam.py
+++ cam.py
@@ -1,59 +1,48 @@
 from gpiozero import OutputDevice as stepper
+from gpiozero.pins.pigpio import PiGPIOFactory
 import face_recognition as fr
 import pigpio
 import cv2
-import os
 
-os.environ["PIGPIO_ADDR"] = "INSERT YOUR PI'S IP ADDRESS HERE"                            
-os.environ["GPIOZERO_PIN_FACTORY"] = "pigpio"
+pi = pigpio.pi('PI IP ADDRESS', 8888)
+if not pi.connected:
+	print('Could not connect to Pi.')
+factory = PiGPIOFactory('PI IP ADDRESS')
 
 FRAME_THICKNESS = 3
 
-me = fr.load_image_file("myFace5.jpg") #THIS FILE WILL BE YOUR FACE
-known = [fr.face_encodings(me)[0]]
 encodings = []
 locations = []
+
 #set () to 1 so it would use external webcam
 video_capture = cv2.VideoCapture(1)
 
-#horizontal motor     REMEMBER THESE ARE THE BCM NUMBERING SYSTEM
-step1_1 = stepper(12)
-step1_2 = stepper(16)
-step1_3 = stepper(20)
-step1_4 = stepper(21)
+#horizontal motor
+step1_1 = stepper(12, pin_factory = factory)
+step1_2 = stepper(16, pin_factory = factory)
+step1_3 = stepper(20, pin_factory = factory)
+step1_4 = stepper(21, pin_factory = factory)
 step1 = (step1_1, step1_2, step1_3, step1_4)
 
-#vertical motor
-step2_1 = stepper(5)
-step2_2 = stepper(6)
-step2_3 = stepper(13)
-step2_4 = stepper(19)
-step2 = (step2_1, step2_2, step2_3, step2_4)
-
-stepping =  [[1,0,0,1],
-             [1,0,0,0], 
-             [1,1,0,0],
-             [0,1,0,0],
+stepping =  [[1,1,0,0],
              [0,1,1,0],
-             [0,0,1,0],
              [0,0,1,1],
-             [0,0,0,1]]
+             [1,0,0,1]]
 
 
-def rotate(num, vert=False):
-	#swivel left and right (up and down if vert) +num for forwards
+def rotate(num):
+	#rotates motor; positive for counterclockwise
 	loopCtrl = 20
 	stepCounter= 0
-	if vert:
-		stepPins = step2
-	else:
-		stepPins = step1
+	stepPins = step1
 	
 	while loopCtrl > 0:
 		for pin in range(0,4):
 		    thisPin = stepPins[pin]         
 		    if stepping[stepCounter][pin] != 0:
-		      thisPin.on()
+		    	thisPin.on()
+		    else:
+		    	thisPin.off()
 		stepCounter += num
 		if (stepCounter >= len(stepping) - 1):
 			stepCounter = 0
@@ -61,39 +50,35 @@ def rotate(num, vert=False):
 			stepCounter = len(stepping) + num
 		loopCtrl -= 1
 
+pseudoFrameCount = 0
 
 while True:
 	ret, frame = video_capture.read()     
 
-	locations = fr.face_locations(frame, model='hog')
-	#set hog to cnn if your computer is beefy
-	encodings = fr.face_encodings(frame, locations)
+	if pseudoFrameCount % 50 == 0:
 
-	for encoding in range(len(encodings)):
-		#find all faces
-		result = fr.compare_faces(known, encodings[encoding], tolerance=0.1)
-		if result:
-			if locations[encoding][0] < 50:
-				rotate(-1, True)
-				print('up')
-			elif locations[encoding][2] > 440:
-				rotate(1, True)
-				print('down')
-			if locations[encoding][3] < 100:
+		locations = fr.face_locations(frame, model='hog')
+		#set hog to cnn if your computer is beefy
+		encodings = fr.face_encodings(frame, locations)
+
+
+		for encoding in range(len(encodings)):
+			#find all faces
+			if locations[encoding][3] < 200:
 				rotate(-1)
-				print('left')
-			elif locations[encoding][1] > 570:
+			elif locations[encoding][1] > 500:
 				rotate(1)
-				print('right')
-			
+
 			center = ((locations[encoding][3]  + locations[encoding][1] )//2, (locations[encoding][0]  + locations[encoding][2] )//2)
 			color = [0, 255, 0]
 			cv2.circle(frame, center, 50, color, FRAME_THICKNESS)
-	
+
 	cv2.imshow('Video', frame)			
-	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+		breakq
+
+	pseudoFrameCount += 1
+
 
 video_capture.release()
 cv2.destroyAllWindows()
